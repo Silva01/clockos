140fa288431c2cf2c74b282f06d3924afae52716
silva01 <silva20102@gmail.com>
2023-05-14 21:08:26 -0300

    Added news configs to pwa

--- vue.config.js
+++ vue.config.js
@@ -2,4 +2,8 @@ const { defineConfig } = require('@vue/cli-service');
 
 module.exports = defineConfig({
   transpileDependencies: true,
+  pwa: {
+    name: 'My App',
+    themeColor: '#ffd100',
+  },
 });
